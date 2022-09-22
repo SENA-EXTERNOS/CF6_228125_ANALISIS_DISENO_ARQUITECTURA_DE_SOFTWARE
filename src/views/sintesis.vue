8b8973d7a6af576335f48a4384f998147b9c9a86
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2022-09-21 23:43:51 -0500

    Actualización formato de créditos.

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -3,8 +3,12 @@
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    p Con el diagrama de actividades se termina la descripción de las tres técnicas usadas para la especificación y el análisis de requisitos, como se pudo analizar cada una de las técnicas nos brinda características para lograr representar gráficamente y de una forma clara, los requisitos que un usuario necesita poder cumplir satisfactoriamente con las solicitudes de los clientes y usuarios, por otro lado se pueden usar todas las técnicas en una especificación y análisis de requisitos si se ve la necesidad.
-    p.mb-5 A continuación, se muestra un mapa conceptual con los elementos más importantes desarrollados en este componente.
+    .titulo-sexto.color-acento-contenido.color-adicional-1.mb-5
+      h5 Desarrollo de aplicaciones web <em>full stack</em>
+      br
+      | Síntesis: análisis, diseño y arquitectura de <em>software</em>
+
+    p.mb-5 En la siguiente síntesis tendrá una mirada resumida de su ruta de conocimiento a través de este componente.
 
     .row.justify-content-center
       .col-lg-10.mb-5
